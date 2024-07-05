d5923acfe289ae82c136f928e200c62213a19220
StefaniaGuta <142820560+StefaniaGuta@users.noreply.github.com>
2024-07-06 02:40:24 +0300

    task done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
+import React from 'react';
+import { useSelector } from 'react-redux';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import styles from './App.module.css';
+
+const App = () => {
+  const contacts = useSelector(state => state.contacts.contacts);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={styles.mainContainer}>
+      <h2>Phonebook</h2>
+      <ContactForm />
+      {contacts.length > 0 ? (
+        <>
+          <h2>Contacts</h2>
+          <ContactList />
+        </>
+      ) : (
+        <h3>No contacts yet</h3>
+      )}
     </div>
   );
 };
+
+export default App;
---
+++ src/components/redux/store.jsx
@@ -0,0 +1,27 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { contactsReducer } from './contactsSlice';
+import { filtersReducer } from './filtersSlice';
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filters: filtersReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['contacts'], 
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
